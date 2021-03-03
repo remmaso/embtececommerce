08ecc0cdca782e5daad51ad75cb4c4e0d1a257db
ralph emmason <remmason@yahoo.com>
2021-03-03 09:46:45 -0800

    first commit

---
+++ frontend/src/screens/ServicesScreen.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container} from 'react-bootstrap'
+
+
+
+const Services = () => {
+    return <div>
+        <Container>
+    <header id="page-header">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 m-auto text-center">
+          <h1>Our Services</h1>
+          <p>Embtec, Providing unique solutions... </p>
+        </div>
+      </div>
+    </div>
+  </header>
+        </Container>
+        </div>
+
+    
+}
+
+export default Services
