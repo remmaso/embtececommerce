561fe6f88cd377c668fc3033da937576b7ed43b9
ralph emmason <remmason@yahoo.com>
2021-03-04 02:56:44 -0800

    changes

--- frontend/src/screens/ServicesScreen.js
+++ frontend/src/screens/ServicesScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container} from 'react-bootstrap'
+import { Container, Col} from 'react-bootstrap'
 
 
 
@@ -16,6 +16,17 @@ const Services = () => {
       </div>
     </div>
   </header>
+  <header id="page-header">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 m-auto text-center">
+          <h1>Our Services</h1>
+          <p>Embtec, Providing unique solutions... </p>
+        </div>
+      </div>
+    </div>
+  </header>
+
         </Container>
         </div>
 
